19f66c9928eddf2bb672ae6b2774fc1a039f9858
julliacastro <julliaoliver14@gmail.com>
2024-06-28 14:03:28 -0300

    testando

--- TodoApi/Services/ITaskService.cs
+++ TodoApi/Services/ITaskService.cs
@@ -20,21 +20,4 @@ public interface ITaskService
     Task DeleteTaskAsync(string id);
 }
 
-/*
- * Porque escolhi o uso de interfaces e serviços:
- * 
- * Interfaces:
- * A utilização de interfaces, como a ITaskService, nos permite definir um contrato que as classes concretas devem seguir.
- * Isso promove uma arquitetura desacoplada, facilitando a substituição de implementações e permitindo a criação de 
- * testes unitários mais eficazes. Com interfaces, podemos facilmente trocar a implementação do serviço sem alterar o 
- * código que o consome, promovendo a flexibilidade e extensibilidade da aplicação.
- * 
- * Serviços:
- * O uso de serviços, como o TaskService que implementa a ITaskService, permite encapsular a lógica de negócios da aplicação.
- * Ao separar a lógica de negócios (serviços) da lógica de acesso a dados (repositórios), obtemos um código mais modular e 
- * fácil de manter. Isso também promove a reutilização de código e facilita a implementação de testes unitários, uma vez 
- * que podemos mockar os serviços e testar a lógica de negócios de forma isolada.
- * 
- * Em resumo, a combinação de interfaces e serviços melhora a estrutura e a qualidade do código, promovendo boas práticas 
- * de programação e facilitando a manutenção e a escalabilidade da aplicação.
- */
+
